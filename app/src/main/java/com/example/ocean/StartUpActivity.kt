14bc987f0ca8970b949c7d3d49af964cbdb1dd00
HungDangQuang <hungdangkg@gmail.com>
2023-04-30 17:45:37 +0700

    styling plash screen

--- app/src/main/java/com/example/ocean/StartUpActivity.kt
+++ app/src/main/java/com/example/ocean/StartUpActivity.kt
@@ -1,9 +1,11 @@
 package com.example.ocean
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
+import kotlinx.coroutines.*
 import java.util.TimerTask
 
 class StartUpActivity : AppCompatActivity() {
@@ -11,5 +13,11 @@ class StartUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start_up)
 
+        runBlocking {
+            GlobalScope.launch(){
+                delay(3000L)
+                startActivity(Intent(this@StartUpActivity,MainActivity::class.java))
+            }
+        }
     }
 }
