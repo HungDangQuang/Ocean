a7787c914e5c784fe80e43acc2fad7b2542b570c
HungDangQuang <hungdangkg@gmail.com>
2023-05-07 18:15:59 +0700

    create logger

---
+++ app/src/main/java/com/example/ocean/Logger.kt
@@ -0,0 +1,20 @@
+package com.example.ocean
+
+import android.util.Log
+
+class Logger(private val TAG: String){
+    private val priority:Int = Log.DEBUG
+
+    public fun log(message:String) : Logger {
+        if (BuildConfig.DEBUG){
+            Log.println(priority,TAG,message)
+        }
+        return this
+    }
+
+    public fun withCause(cause:Exception){
+        if (BuildConfig.DEBUG){
+            Log.println(priority,TAG,Log.getStackTraceString(cause))
+        }
+    }
+}
