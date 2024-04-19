be188103f234f502ec540dd373dfde22f19006a8
HungDangQuang <hungdangkg@gmail.com>
2024-04-19 20:18:47 +0700

    set up dagger hilt for Application class

--- app/src/main/java/com/example/ocean/OceanApplication.kt
+++ app/src/main/java/com/example/ocean/OceanApplication.kt
@@ -1,7 +1,9 @@
 package com.example.ocean
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class OceanApplication : Application() {
 
     companion object {
