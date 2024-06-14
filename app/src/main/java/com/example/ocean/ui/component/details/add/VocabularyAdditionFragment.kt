95bffdc005b095355c331252bba209d7ec8b5c46
HungDangQuang <hungdangkg@gmail.com>
2024-06-14 14:45:19 +0700

    remove viewModel implementation

--- app/src/main/java/com/example/ocean/ui/component/details/add/VocabularyAdditionFragment.kt
+++ app/src/main/java/com/example/ocean/ui/component/details/add/VocabularyAdditionFragment.kt
@@ -1,7 +1,6 @@
 package com.example.ocean.ui.component.details.add
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,21 +8,14 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.ocean.CountryViewModel
 import com.example.ocean.R
-import com.example.ocean.Utils.Utility
 import com.example.ocean.data.Country
-import com.example.ocean.data.CountryRepositoryImpl
-import com.example.ocean.data.RetrofitInstance
 import com.example.ocean.databinding.FragmentVocabularyAdditionBinding
-import com.example.ocean.domain.storage.StorageUtils
-import com.example.ocean.domain.usecase.GetCountryUseCase
 import com.example.ocean.ui.base.BaseFragment
-import com.example.presentation.CountryViewModelFactory
 
-class VocabularyAdditionFragment : BaseFragment(), StorageUtils {
+class VocabularyAdditionFragment : BaseFragment() {
     private lateinit var binding:FragmentVocabularyAdditionBinding
     private lateinit var countryViewModel: CountryViewModel
     private val TAG = VocabularyAdditionFragment::class.java.simpleName
-    private lateinit var vm: com.example.presentation.CountryViewModel
 
     override fun createView(
         inflater: LayoutInflater,
@@ -50,17 +42,7 @@ class VocabularyAdditionFragment : BaseFragment(), StorageUtils {
 
         countryViewModel.inputLanguageCountry.value = Country(R.drawable.united_states_flag, "USA")
         countryViewModel.outputLanguageCountry.value = Country(R.drawable.united_kingdom_flag, "UK")
-        setUpViewModel()
     }
-
-    private fun setUpViewModel() {
-        val countryRepository = CountryRepositoryImpl(RetrofitInstance.apiService)
-        val getCountriesUseCase = GetCountryUseCase(countryRepository,"AD" ,this)
-        val viewModelFactory = CountryViewModelFactory(getCountriesUseCase)
-        vm = ViewModelProvider(this, viewModelFactory)[com.example.presentation.CountryViewModel::class.java]
-
-    }
-
 
     override fun setUpClickableView() {
 
@@ -68,10 +50,6 @@ class VocabularyAdditionFragment : BaseFragment(), StorageUtils {
 
     override fun goToNextScreen() {
 
-    }
-
-    override fun storeFileInLocalStorage(byteArray: ByteArray, fileName: String) {
-        context?.let { Utility.saveImageToDisk(it, byteArray, fileName) }
     }
 
 }
