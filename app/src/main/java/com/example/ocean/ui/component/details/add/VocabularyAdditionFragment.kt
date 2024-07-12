85be68c9e8fcaa13432397c94c63921e2aecde20
HungDangQuang <hungdangkg@gmail.com>
2024-07-12 13:50:16 +0700

    Add loading and translated result UIs

--- app/src/main/java/com/example/ocean/ui/component/details/add/VocabularyAdditionFragment.kt
+++ app/src/main/java/com/example/ocean/ui/component/details/add/VocabularyAdditionFragment.kt
@@ -5,6 +5,8 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.ocean.R
@@ -55,11 +57,44 @@ class VocabularyAdditionFragment : BaseFragment() {
         }
 
         binding.btTranslate.setOnClickListener {
-            countryListViewModel.translateText(binding.tiInputText.text.toString())
+            binding.tiInputText.onEditorAction(EditorInfo.IME_ACTION_DONE)
+            if (isTextInputEmpty()) {
+                Toast.makeText(requireContext(), R.string.toast_message_no_input, Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                // translate text
+                countryListViewModel.translateText(binding.tiInputText.text.toString())
+
+                // perform animation
+                if (binding.clOutputText.visibility == View.GONE) {
+                    binding.progressBar.visibility = View.VISIBLE
+                    binding.progressBar.alpha = 0f
+                    Log.d(TAG, "setUpClickableView: ${binding.progressBar.height.toFloat()}")
+                    binding.progressBar.translationY = -binding.progressBar.height.toFloat()
+                    binding.progressBar.animate()
+                        .translationY(0f)
+                        .alpha(1f)
+                        .setDuration(500)
+                        .start()
+                } else {
+                    it.animate()
+                        .alpha(0f)
+                        .setDuration(200)
+                        .withEndAction {
+                            it.visibility = View.INVISIBLE
+                            binding.smallProgressBar.visibility = View.VISIBLE
+                        }
+                }
+            }
         }
 
         binding.btDeleteText.setOnClickListener {
             binding.tiInputText.setText("")
+        }
+
+        binding.btDeleteOutputText.setOnClickListener {
+            binding.clOutputText.visibility = View.GONE
+            binding.tvOutputText.text = ""
         }
     }
 
@@ -117,8 +152,33 @@ class VocabularyAdditionFragment : BaseFragment() {
 
         countryListViewModel.translatedText.observe(viewLifecycleOwner) {
             Log.d(TAG, "setUpCountryListViewModel: translated text $it")
+            // set the output text
+            binding.tvOutputText.text = it
+
+            // perform animation
+            if (binding.clOutputText.visibility == View.GONE) {
+                binding.clOutputText.visibility = View.VISIBLE
+                binding.clOutputText.alpha = 0f
+                binding.progressBar.visibility = View.GONE
+                binding.clOutputText.animate()
+                    .alpha(1f)
+                    .setDuration(500)
+                    .start()
+            } else {
+                binding.btTranslate.visibility = View.VISIBLE
+                binding.btTranslate.animate()
+                    .alpha(1f)
+                    .setDuration(200)
+                    .withEndAction {
+                        binding.smallProgressBar.visibility = View.GONE
+                    }
+            }
         }
 
+    }
+
+    private fun isTextInputEmpty(): Boolean {
+        return binding.tiInputText.text?.length == 0
     }
 
 }
