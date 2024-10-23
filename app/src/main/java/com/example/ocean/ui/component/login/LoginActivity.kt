17408a3ae7807762b1b3d9a67160794a6031dfd1
HungDangQuang <hungdangkg@gmail.com>
2024-10-23 11:13:19 +0700

    Test Sign in using GoogleSignInOptions

--- app/src/main/java/com/example/ocean/ui/component/login/LoginActivity.kt
+++ app/src/main/java/com/example/ocean/ui/component/login/LoginActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
-import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -13,17 +12,14 @@ import com.example.ocean.R
 import com.example.ocean.Utils.Utility.Companion.isValidEmail
 import com.example.ocean.databinding.ActivityLoginBinding
 import com.example.ocean.presentation.LoginViewModel
-import com.google.android.gms.auth.api.identity.BeginSignInRequest
-import com.google.android.gms.auth.api.identity.Identity
-import com.google.android.gms.auth.api.identity.SignInClient
-import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivityLoginBinding
-    private lateinit var oneTapClient: SignInClient
     private val loginViewModel: LoginViewModel by viewModels()
     private val TAG = LoginActivity::class.java.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,55 +46,18 @@ class LoginActivity : AppCompatActivity() {
             // check password error
             checkPasswordError()
         }
+
+        binding.ivGoogle.setOnClickListener {
+            testLogin()
+        }
     }
 
-    override fun onStart() {
-        super.onStart()
-        // Initialize One Tap client
-        oneTapClient = Identity.getSignInClient(this)
-        // Trigger One Tap Sign-In
-        startOneTapSignIn()
-    }
-
-    private fun startOneTapSignIn() {
-        val signInRequest = BeginSignInRequest.builder()
-            .setGoogleIdTokenRequestOptions(
-                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-                    .setSupported(true)
-                    // Your server's client ID, not your Android client ID.
-                    .setServerClientId("251418567267-c14gfekqiedspb0n02dm4ss5u4pf1rmj.apps.googleusercontent.com")
-                    // Only show accounts previously used to sign in.
-                    .setFilterByAuthorizedAccounts(false)
-                    .build()
-            )
-            .build()
-
-        oneTapClient.beginSignIn(signInRequest)
-            .addOnSuccessListener(this) { result ->
-                try {
-                    oneTapSignInLauncher.launch(IntentSenderRequest.Builder(result.pendingIntent.intentSender).build())
-                } catch (e: Exception) {
-                    Log.e(ContentValues.TAG, "Error launching One Tap sign-in", e)
-                }
-            }
-            .addOnFailureListener(this) { e ->
-                Log.d(ContentValues.TAG, "One Tap sign-in failed")
-                Log.d(ContentValues.TAG, "error: $e")
-            }
-    }
-
-    private val oneTapSignInLauncher = registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result ->
+    private val signInLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
         if (result.resultCode == RESULT_OK) {
             try {
-                val credential = oneTapClient.getSignInCredentialFromIntent(result.data)
-                val idToken = credential.googleIdToken
-                if (idToken != null) {
-                    loginViewModel.loginWithGoogleEmail(idToken)
-                } else {
-                    Log.e(ContentValues.TAG, "No ID token found!")
-                }
-            } catch (e: ApiException) {
-                Log.e(ContentValues.TAG, "One Tap sign-in failed", e)
+                Log.d(TAG, "Login successfully")
+            } catch (e: Exception) {
+                Log.e(ContentValues.TAG, "Sign-in failed", e)
             }
         }
     }
@@ -123,4 +82,16 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
+    private fun testLogin() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .requestIdToken("251418567267-c14gfekqiedspb0n02dm4ss5u4pf1rmj.apps.googleusercontent.com")
+            .build()
+
+        val googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+
+        val signInIntent = googleSignInClient.signInIntent
+        signInLauncher.launch(signInIntent)
+    }
 }
